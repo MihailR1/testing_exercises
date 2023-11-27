787bb0f059ced7209e7a0b1b309da9692588d35e
MihailR <MihailR.20@yandex.ru>
2023-11-27 14:44:25 +0500

    Test level 2

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -1,2 +1,51 @@
+import pytest
+
 from functions.level_2.four_sentiment import check_tweet_sentiment
 
+"""
+Случаи для тестирования:
+Успешные:
+    1 - Стандартные случаи (4шт): Текст и списки плохих, хороших слов - отработка Good, bad, 2x - None
+    
+Крайние случаи:
+    1 - Слово, которое ищем, находится в конце предложения и заканчивается точкой ". или !"
+    2 - Ошибочный тип данных good/bad words, вместо set подали list - тест отработает
+    3 - Ошибочный тип данных в text - тест упадет, AttributeErr
+"""
+
+
+class TestCheckTweetSentiment:
+    text = 'Hello world this is default test for pytest. Simple test for pytest!'
+    good_words = {'world', 'hello'}
+    bad_words = {'is', 'this'}
+
+    @pytest.mark.parametrize(
+        'good_words, bad_words, result',
+        [
+            (good_words, bad_words, None),
+            ({'no words'}, {'no words'}, None),
+            ({'world', 'hello', 'test'}, bad_words, 'GOOD'),
+            (good_words, {'world', 'hello', 'test'}, 'BAD')
+        ]
+    )
+    def test__check_tweet_sentiment__standart_success_cases(self, good_words, bad_words, result):
+        assert check_tweet_sentiment(self.text, good_words, bad_words) == result
+
+    def test__check_tweet_sentiment__word_endwith_dot(self):
+        good_words = self.good_words
+        good_words.add('pytest')
+        expected_result = None
+
+        assert check_tweet_sentiment(self.text, good_words, self.bad_words) == expected_result
+
+    def test__check_tweet_sentiment__wrong_type_data_in_good_words(self):
+        good_words = list(self.good_words)
+        expected_result = None
+
+        assert check_tweet_sentiment(self.text, good_words, self.bad_words) == expected_result
+
+    def test__check_tweet_sentiment__wrong_type_data_in_text(self):
+        text = list(self.text)
+
+        with pytest.raises(AttributeError):
+            assert check_tweet_sentiment(text, self.good_words, self.bad_words)
--- tests/level_2/test_one_pr_url.py
+++ tests/level_2/test_one_pr_url.py
@@ -1,1 +1,27 @@
+import pytest
+
 from functions.level_2.one_pr_url import is_github_pull_request_url
+
+"""
+Случаи для тестирования:
+Успешные:
+    1 - Стандартный, успешный случай - вернет True
+    2 - Ссылка на файл в pull_request - вернет false
+    3 - Ссылка на другой домен - false
+    4 - Некорректная ссылка - False
+Крайние случаи:
+    Что тут может быть крайним случаем понять не могу
+"""
+
+
+@pytest.mark.parametrize(
+    'url, result',
+    [
+        (f'https://github.com/learnpythonru/oop_bases_challenges/pull/20', True),
+        (f'https://github.com/learnpythonru/oop_bases_challenges/pull/20/files', False),
+        (f'https://gitlab.com/learnpythonru/oop_bases_challenges/pull/20', False),
+        (f'github.com/learnpythonru/oop_bases_challenges/pull/20', False)
+    ]
+)
+def test__is_github_pull_request_url__all_cases(url, result):
+    assert is_github_pull_request_url(url) is result
