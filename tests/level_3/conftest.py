846941d0fc9f5dc42ed0b34036461a662b29a94a
MihailR <MihailR.20@yandex.ru>
2023-12-15 07:54:39 +0500

    add conftest.py

--- tests/level_3/conftest.py
+++ tests/level_3/conftest.py
@@ -1,5 +1,6 @@
 import datetime
 import decimal
+import random
 
 import pytest
 
@@ -21,13 +22,30 @@ def sms_message():
 
 
 @pytest.fixture
+def popular_stores():
+    return ['ozon.ru', 'amazon.ru', 'wildberries.ru', 'walmart.ru', 'yandex.market.ru', 'store.ru',
+            'mcdonalds', 'burgerking', 'burgerfree', 'kebabstore', 'lavash&co', 'блинная',
+            'lego-store', 'apteka', 'marvel', 'cinemapark']
+
+
+@pytest.fixture
+def different_dates():
+    return [
+        datetime.datetime(2023, random.randint(10, 11),
+                          random.randint(1, 30),
+                          random.randint(0, 23)
+                          ) for _ in range(10)
+    ]
+
+
+@pytest.fixture
 def create_expense():
     def expense(
             amount: decimal.Decimal = decimal.Decimal(430.5),
             currency: Currency = Currency.RUB,
             card: BankCard = BankCard(last_digits='4422', owner='Will Smith'),
             spent_in: str = 'amazon',
-            spent_at: datetime.datetime = datetime.datetime.utcnow(),
+            spent_at: datetime.datetime = datetime.datetime(2023, 11, 11, 11, 40),
             category: ExpenseCategory | None = ExpenseCategory.SUPERMARKET
     ):
         created_expense = Expense(
@@ -42,3 +60,20 @@ def create_expense():
 
     yield expense
     del expense
+
+
+@pytest.fixture
+def list_expenses_without_fraud_transactions(
+        create_expense, popular_stores, different_dates):
+    return [
+        create_expense(
+            spent_in=random.choice(popular_stores),
+            spent_at=random.choice(different_dates),
+            amount=decimal.Decimal(random.randint(10, 4999))) for _ in range(10)
+    ]
+
+
+@pytest.fixture
+def list_expenses_with_fraud_transactions(list_expenses_without_fraud_transactions):
+    choice_random = random.choices(list_expenses_without_fraud_transactions, k=2) * 3
+    return list_expenses_without_fraud_transactions + choice_random
--- tests/level_3/test_four_fraud.py
+++ tests/level_3/test_four_fraud.py
@@ -1,10 +1,16 @@
-import pytest
+import random
 
 from functions.level_3.four_fraud import find_fraud_expenses
-from functions.level_3.models import Expense
 
 
-def test__find_fraud_expenses__test(create_expense):
-    expense: Expense = create_expense()
-    print(expense)
-    print(expense.card.last_digits)
+def test__find_fraud_expenses__without_fraud(list_expenses_without_fraud_transactions):
+    result = find_fraud_expenses(list_expenses_without_fraud_transactions)
+
+    assert result == []
+
+
+def test__find_fraud_expenses__with_fraud(list_expenses_with_fraud_transactions):
+    result = find_fraud_expenses(list_expenses_with_fraud_transactions)
+
+    assert result != []
+
